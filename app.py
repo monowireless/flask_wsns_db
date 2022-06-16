2ae66162a980dc0109fb675206500e47b098b3cd
Seigo(成吾) Okumura(奥村) <seigo.okumura@mono-wireless.com>
2022-06-16 12:49:39 +0900

    source maintenance (functioning, commenting)

--- app.py
+++ app.py
@@ -1,26 +1,48 @@
+#########################################################################################
 # Copyright (C) 2022 Mono Wireless Inc. All Rights Reserved.
 # Released under MW-OSSLA-1J,1E (MONO WIRELESS OPEN SOURCE SOFTWARE LICENSE AGREEMENT).
+#########################################################################################
 
-import sys
-import sqlite3
+#########################################################################################
+# IMPORTS
+#########################################################################################
+# generic modules
 from datetime import datetime
-from flask import Flask,render_template,request,g
-
 import base64
 from io import BytesIO
 
-from flask import Flask
+# sqlite3 (Lightweight SQL database)
+import sqlite3
+
+# Flask (Lighweight HTTP server)
+from flask import Flask,render_template,request,g
+
+# matplotlib (Graphing)
 from matplotlib.figure import Figure
 import matplotlib.dates as mdates
 
-# configs
+#########################################################################################
+# VARIABLES
+#########################################################################################
+# app object of Flask
+app = Flask(__name__)
+
+### configs (config.ini)
+# example:
+#     [SQLITE3]
+#     db_file = ../log/your_db_file.sqlite
 import configparser
 config = configparser.ConfigParser()
 config.read('config.ini')
 try: conf_db_filename = config['SQLITE3']['db_file']
 except: conf_db_filename = '../log/TWELITE_Stage_WSns.sqlite'
 
-# some dictionaries 
+#########################################################################################
+# DICTIONARIES
+#########################################################################################
+
+# labels by paket ID
+# OR packet ID by string label.
 dict_pkt_type = {
     1 : ('PAL MAG', 'MAG', 'N/A', 'N/A', 'N/A'),
     2 : ('PAL AMB', '温度[℃]', '湿度[%]', '照度[lx]', 'N/A'),
@@ -36,6 +58,17 @@ dict_pkt_type = {
     'APPTWELITE' : 257
 }
 
+# labels for graph plotting (must not include non-ascii chars)
+dict_label = {
+    1 : ('PAL MAG', 'MAG', 'N/A', 'N/A', 'N/A'),
+    2 : ('PAL AMB', 'TEMP[C]', 'HUMID[%]', 'LUMI[lx]', 'N/A'),
+    3 : ('PAL MOT','X[G]', 'Y[G]', 'Z[G]', 'N/A'),
+    5 : ('CUE','X[G]', 'Y[G]', 'Z[G]', 'N/A'),
+    6 : ('ARIA','TEMP[C]', 'HUMID[%]', 'N/A', 'N/A'),
+    257 : ('App_TWELITE','DI1/2/3/4', 'AD1[V]', 'AD2[V]', 'AD3[V]'),
+}
+
+# Label string corresponding to MAG data
 dict_mag = {
     0 : 'なし',
     1 : 'N極',
@@ -43,6 +76,8 @@ dict_mag = {
     3 :  None
 }
 
+# Label string corresponding to DIO bitmap data.
+#   configure the table to display H/L in the order of D1/D2/D3/D4
 dict_dio = {
     0 : 'H/H/H/H',
     1 : 'L/H/H/H',
@@ -62,19 +97,21 @@ dict_dio = {
     15: 'L/L/L/L',
 }
 
+# Label string corresponding to CUE event data.
 dict_ev_cue = {
-    1 : '面１',
+    1 : '面１', # 1 of the dice face 
     2 : '面２',
     3 : '面３',
     4 : '面４',
     5 : '面５',
     6 : '面６',
-    8 : 'シェイク',
-    16 : 'ムーブ',
+    8 : 'シェイク', # shaking
+    16 : 'ムーブ', # moving
 }
 
-# app def
-app = Flask(__name__)
+#########################################################################################
+# SUB ROUTINES
+#########################################################################################
 
 # convert integer value into hex value (assuming int32_t)
 def tohex_i32(val):
@@ -93,7 +130,114 @@ def db_open():
         g.db = sqlite3.connect(conf_db_filename)
     return g.db
 
-# query nodes and desc (/)
+# plot a fig.
+# @ax       subplot object.
+# @v_time   list of X-AXIS (time)
+# @v_data   list of Y-AXIS (data)
+# @lbl      label of data
+# @col      color of plotting line
+# @returns   NONE
+def _graph_plot(ax, v_time, v_data, lbl, col):
+        ax.tick_params(labelsize = 6.5) 
+        ax.plot(v_time, v_data, label=lbl, color=col)
+        #ax.set_ylabel(lbl)
+        ax.legend(loc='upper left')
+        ax.xaxis.set_major_formatter(mdates.DateFormatter("%H:%M"))
+
+# output fig as <img...> tag with embedded data.
+# @fig      figure object which is already plotted.
+# @returns  image tag data with data.
+def _graph_get_img_tab_embedded_data(fig):
+    buf = BytesIO()
+    fig.savefig(buf, format="png")
+    # Embed the result in the html output.
+    data = base64.b64encode(buf.getbuffer()).decode("ascii")
+    return f"<img src='data:image/png;base64,{data}'/>"
+
+# grenrate graph data.
+# @param latest_ts              if not 0, render graph from latest_ts - 1day to latest_ts.
+# @param yera, month, day       specify YYYY/MM/DD (where latest_ts==0)
+def _graph_a_day(sid, i32sid, latest_ts, year, month, day):
+    global dict_label
+
+    # open data base and query
+    con = db_open()
+    cur = con.cursor()
+    if latest_ts == 0:
+        cur.execute('''SELECT ts,lid,lqi,pkt_type,value,value1,value2,value3,val_vcc_mv,val_dio,ev_id FROM sensor_data
+                    WHERE (sid=?) and (year=?) and (month=?) and (day=?)
+                    ORDER BY random() LIMIT 1024''', (i32sid,year,month,day,))
+    else:
+        cur.execute('''SELECT ts,lid,lqi,pkt_type,value,value1,value2,value3,val_vcc_mv,val_dio,ev_id FROM sensor_data
+                    WHERE (sid=?) and (ts BETWEEN ? and ?)
+                    ORDER BY random() LIMIT 1024''', (i32sid,latest_ts-86399,latest_ts,))
+        lt = datetime.fromtimestamp(latest_ts) # convert unix epoch timestamp to local time object.
+        year = lt.year
+        month = lt.month
+        day = lt.day
+        
+    r = cur.fetchall()
+    con.close()
+
+    # check first sample (determine packet type, etc)
+    lblinfo = ('UNKNOWN', 'VAL', 'VAL1', 'VAL2', 'VAL3')
+    pkt_type = None
+    lid = None
+    if len(r) > 0:
+        try:
+            # pick first sample
+            r0 = r[0]
+            # packet type
+            pkt_type = int(r0[3])
+            if pkt_type in dict_label: lblinfo=dict_label[pkt_type]
+            # logical ID (normally, all the same)
+            lid = r0[1]
+        except:
+            pass
+
+    # list for plotting.
+    v_t = [] # X-AXIS (time)
+    v_0 = []
+    v_1 = []
+    v_2 = []
+    
+    # sorting the list (use random pick during SQL query, but sorted by ts is better for grapphing)
+    sr = sorted(r, key=lambda x : x[0])
+    ct = 1
+    for ts,lid,lqi,pkt_type,value,value1,value2,value3,val_vcc_mv,val_dio,ev_id in sr:
+        lt = datetime.fromtimestamp(ts) # convert unix epoch timestamp to local time object.
+        v_0.append(value)
+        v_1.append(value1)
+        v_2.append(value2)
+        v_t.append(lt)
+
+    ### plotting figs.
+    fig = Figure()
+    fig.set_size_inches(5, 10)
+    fig.suptitle("%s - %04d/%02d/%02d - %s" % (sid, int(year), int(month), int(day), lblinfo[0]))
+    
+    # first fig
+    _graph_plot(fig.add_subplot(3, 1, 1), v_t, v_0, lblinfo[1], 'red')
+
+    # second fig
+    if v_1.count(None) < len(v_1):
+        _graph_plot(fig.add_subplot(3, 1, 2), v_t, v_1, lblinfo[2], 'green')
+        
+    # 3rd fig
+    if v_2.count(None) < len(v_2):
+        _graph_plot(fig.add_subplot(3, 1, 3), v_t, v_1, lblinfo[3], 'blue')
+
+    # set layout strategy   
+    fig.tight_layout()  
+
+    return _graph_get_img_tab_embedded_data(fig)
+
+#########################################################################################
+# HTTP REQUEST HANDLERS
+#########################################################################################
+
+### Handler for address '/'
+# Enumerate SIDs where sensor data exists.
 @app.route('/')
 def index():
     global dict_pkt_type
@@ -134,12 +278,13 @@ def index():
     # returns
     return render_template('index.html', data = result)
 
-# query years
+
+### Handler for POST request of the address '/year'.
+# Enumerate the years for which sensor data exist from the specified SID.
 @app.route('/year', methods=["POST"])
 def list_years():
     sid = request.form['sid']
     i32sid = request.form['i32sid']
-    latest_ts = request.form['latest_ts']
     desc = request.form["desc"]
 
     # open data base and query
@@ -150,12 +295,12 @@ def list_years():
     con.close()
     return render_template('year.html', sid = sid, i32sid = i32sid, desc = desc, data = result)
 
-# query months
+### Handler for POST request of the address '/month'.
+# Enumerates the months in which data exist from the specified SID and year
 @app.route('/month', methods=["POST"])
 def list_months():
     sid = request.form['sid']
     i32sid = request.form['i32sid']
-    latest_ts = request.form['latest_ts']
     desc = request.form["desc"]
     year = request.form["year"]
     
@@ -167,12 +312,12 @@ def list_months():
     con.close()
     return render_template('month.html', sid = sid, i32sid = i32sid, desc = desc, year = year, data = result)
 
-# query days 
+### Handler for POST request of the address '/day'
+# Enumerates the days for which data exist from the specified SID, year, and month.
 @app.route('/day', methods=["POST"])
 def list_days():
     sid = request.form['sid']
     i32sid = request.form['i32sid']
-    latest_ts = request.form['latest_ts']
     desc = request.form["desc"]
     year = request.form["year"]
     month = request.form["month"]
@@ -186,14 +331,14 @@ def list_days():
     con.close()
     return render_template('day.html', sid = sid, i32sid = i32sid, desc = desc, year = year, month=month, data = result)
 
-# query sensor data for the day.
+### Handler for POST request of the address '/show_the_day'.
+# Enumerate the list of sensor data present for the specified SID, year, month, and day.
 @app.route('/show_the_day', methods=["POST"])
 def show_the_day():
     global dict_pkt_type, dict_mag, dict_dio, dict_ev_cue
 
     sid = request.form['sid']
     i32sid = request.form['i32sid']
-    latest_ts = request.form['latest_ts']
     desc = request.form["desc"]
     year = request.form["year"]
     month = request.form["month"]
@@ -227,7 +372,7 @@ def show_the_day():
     ct = 0
     for ts,lid,lqi,pkt_type,value,value1,value2,value3,val_vcc_mv,val_dio,ev_id in r:
         try:
-            lt = datetime.fromtimestamp(ts)
+            lt = datetime.fromtimestamp(ts) # convert unix epoch timestamp to local time object.
 
             # mag info
             mag = None
@@ -259,107 +404,8 @@ def show_the_day():
                 sid = sid, i32sid = i32sid, desc = desc, year = year, 
                 month=month, day=day, data = result, lid=lid, lblinfo=lblinfo)
 
-# grenrate graph data.
-# @param latest_ts              if not 0, render graph from latest_ts - 1day to latest_ts.
-# @param yera, month, day       specify YYYY/MM/DD (where latest_ts==0)
-def _graph_a_day(sid, i32sid, latest_ts, year, month, day):
-    dict_label = {
-        1 : ('PAL MAG', 'MAG', 'N/A', 'N/A', 'N/A'),
-        2 : ('PAL AMB', 'TEMP[C]', 'HUMID[%]', 'LUMI[lx]', 'N/A'),
-        3 : ('PAL MOT','X[G]', 'Y[G]', 'Z[G]', 'N/A'),
-        5 : ('CUE','X[G]', 'Y[G]', 'Z[G]', 'N/A'),
-        6 : ('ARIA','TEMP[C]', 'HUMID[%]', 'N/A', 'N/A'),
-        257 : ('App_TWELITE','DI1/2/3/4', 'AD1[V]', 'AD2[V]', 'AD3[V]'),
-    }
-
-    # open data base and query
-    con = db_open()
-    cur = con.cursor()
-    if latest_ts == 0:
-        cur.execute('''SELECT ts,lid,lqi,pkt_type,value,value1,value2,value3,val_vcc_mv,val_dio,ev_id FROM sensor_data
-                    WHERE (sid=?) and (year=?) and (month=?) and (day=?)
-                    ORDER BY random() LIMIT 1024''', (i32sid,year,month,day,))
-    else:
-        cur.execute('''SELECT ts,lid,lqi,pkt_type,value,value1,value2,value3,val_vcc_mv,val_dio,ev_id FROM sensor_data
-                    WHERE (sid=?) and (ts BETWEEN ? and ?)
-                    ORDER BY random() LIMIT 1024''', (i32sid,latest_ts-86399,latest_ts,))
-        lt = datetime.fromtimestamp(latest_ts)
-        year = lt.year
-        month = lt.month
-        day = lt.day
-        
-    r = cur.fetchall()
-    con.close()
-
-    # check first sample (determine packet type, etc)
-    lblinfo = ('UNKNOWN', 'VAL', 'VAL1', 'VAL2', 'VAL3')
-    pkt_type = None
-    lid = None
-    if len(r) > 0:
-        try:
-            # pick first sample
-            r0 = r[0]
-            # packet type
-            pkt_type = int(r0[3])
-            if pkt_type in dict_label: lblinfo=dict_label[pkt_type]
-            # logical ID (normally, all the same)
-            lid = r0[1]
-        except:
-            pass
-
-    # save vector
-    v_0 = []
-    v_1 = []
-    v_2 = []
-    v_t = []
-    
-    # sorting the list (use random pick during SQL query, but sorted by ts is better for grapphing)
-    sr = sorted(r, key=lambda x : x[0])
-    ct = 1
-    for ts,lid,lqi,pkt_type,value,value1,value2,value3,val_vcc_mv,val_dio,ev_id in sr:
-        lt = datetime.fromtimestamp(ts)
-        v_0.append(value)
-        v_1.append(value1)
-        v_2.append(value2)
-        v_t.append(lt)
-
-    ### save fig
-    fig = Figure()
-    fig.set_size_inches(5, 10)
-    fig.suptitle("%s - %04d/%02d/%02d - %s" % (sid, int(year), int(month), int(day), lblinfo[0]))
-    
-    ax = fig.add_subplot(3, 1, 1)
-    ax.tick_params(labelsize = 6.5) 
-    ax.plot(v_t, v_0, label=lblinfo[1], color='red')
-    #ax.set_ylabel(lblinfo[1])
-    ax.legend(loc='upper left')
-    ax.xaxis.set_major_formatter(mdates.DateFormatter("%H:%M"))
-
-    if v_1.count(None) != len(v_1):
-        ax1 = fig.add_subplot(3, 1, 2)
-        ax1.tick_params(labelsize = 6.5) 
-        ax1.plot(v_t, v_1, label=lblinfo[2], color='green')
-        #ax1.set_ylabel(lblinfo[2])
-        ax1.legend(loc='upper left')
-        ax1.xaxis.set_major_formatter(mdates.DateFormatter("%H:%M"))
-
-    if v_2.count(None) != len(v_2):
-        ax2 = fig.add_subplot(3, 1, 3)
-        ax2.tick_params(labelsize = 6.5) 
-        ax2.plot(v_t, v_2, label=lblinfo[3], color='blue')
-        #ax2.set_ylabel(lblinfo[3])
-        ax2.legend(loc='upper left')
-        ax2.xaxis.set_major_formatter(mdates.DateFormatter("%H:%M"))
-    
-    fig.tight_layout()  
-
-    buf = BytesIO()
-    fig.savefig(buf, format="png")
-    # Embed the result in the html output.
-    data = base64.b64encode(buf.getbuffer()).decode("ascii")
-    return f"<img src='data:image/png;base64,{data}'/>"
-
-# query sensor data for the day.
+### Handler for POST request of the address '/graph_the_day'.
+# Plot a graph of sensor data for a specified date.
 @app.route('/graph_the_day', methods=["POST"])
 def graph_the_day():
     sid = request.form['sid']
@@ -372,7 +418,8 @@ def graph_the_day():
 
     return _graph_a_day(sid, i32sid, 0, year, month, day)
 
-# query sensor data of the latest 24hours
+### Handler for POST request of the address '/graph_the_latest'.
+# Plot a graph of sensor data going back 24 hours from the specified timestamp
 @app.route('/graph_the_latest', methods=["POST"])
 def graph_the_latest():
     sid = request.form['sid']
@@ -382,6 +429,9 @@ def graph_the_latest():
 
     return _graph_a_day(sid, i32sid, int(latest_ts), 0, 0, 0)
 
+#########################################################################################
+# RUN THE APP
+#########################################################################################
 if __name__ == '__main__':
     app.debug = True
     app.run(host='localhost')
